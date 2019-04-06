bfec32a3b922b7d6751b23ad3da2c30fb5adb04e
cybervation team <cybervation@users.noreply.github.com>
2019-04-06 17:51:04 +0530

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -780,6 +780,11 @@ function InitPushNotification(storeId) {
     });
 
     push.on('notification', function (data) {
+        setTimeout(function () {
+            stopAudio();
+            acceptOrderPopup.destroy();
+            // Do something after 30 second 
+        }, 30000);
         //alert('notification event: ' + data.message);
         CheckNewOrder();
         // alert('notification event: ' + data.message + ", " + data.title);
