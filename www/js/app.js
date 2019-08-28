285d232bfb6cac8057f4c0209ebcf580b1498949
projpro <48902384+projpro@users.noreply.github.com>
2019-08-28 21:19:39 +0530

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -485,7 +485,7 @@ $$(document).on('page:init', function (e) {
          {
              preferFrontCamera: false, // iOS and Android
              showFlipCameraButton: true, // iOS and Android
-             formats: "CODE_128"
+             formats: "CODE_128,CODE_39,CODE_93,CODABAR"
          }
 
          );
@@ -504,13 +504,13 @@ $$(document).on('page:init', function (e) {
           {
               preferFrontCamera: false, // iOS and Android
               showFlipCameraButton: true, // iOS and Android
-              showTorchButton: false, // iOS and Android
+              showTorchButton: true, // iOS and Android
               torchOn: false, // Android, launch with the torch switched on (if available)
               saveHistory: true, // Android, save scan history (default false)
               prompt: "Place a barcode inside the scan area", // Android
               resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
               //formats: "QR_CODE,PDF_417,CODABAR,CODE_128,CODE_93,CODE_39", // default: all but PDF_417 and RSS_EXPANDED
-              orientation: "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+              //orientation: "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
               disableAnimations: true, // iOS
               disableSuccessBeep: false // iOS and Android
           }
@@ -531,13 +531,13 @@ $$(document).on('page:init', function (e) {
           {
               preferFrontCamera: false, // iOS and Android
               showFlipCameraButton: true, // iOS and Android
-              showTorchButton: false, // iOS and Android
+              showTorchButton: true, // iOS and Android
               torchOn: false, // Android, launch with the torch switched on (if available)
               saveHistory: true, // Android, save scan history (default false)
               prompt: "Place a barcode inside the scan area", // Android
               resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
               //formats: "QR_CODE,PDF_417,CODABAR,CODE_128,CODE_93,CODE_39", // default: all but PDF_417 and RSS_EXPANDED
-              orientation: "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+              //orientation: "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
               disableAnimations: true, // iOS
               disableSuccessBeep: false // iOS and Android
           }
@@ -837,13 +837,13 @@ $$(document).on('page:init', function (e) {
           {
               preferFrontCamera: false, // iOS and Android
               showFlipCameraButton: true, // iOS and Android
-              showTorchButton: false, // iOS and Android
+              showTorchButton: true, // iOS and Android
               torchOn: false, // Android, launch with the torch switched on (if available)
               saveHistory: true, // Android, save scan history (default false)
               prompt: "Place a barcode inside the scan area", // Android
               resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
               //formats: "QR_CODE,PDF_417,CODABAR,CODE_128,CODE_93,CODE_39", // default: all but PDF_417 and RSS_EXPANDED
-              orientation: "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+              //orientation: "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
               disableAnimations: true, // iOS
               disableSuccessBeep: false // iOS and Android
           }
@@ -864,13 +864,13 @@ $$(document).on('page:init', function (e) {
           {
               preferFrontCamera: false, // iOS and Android
               showFlipCameraButton: true, // iOS and Android
-              showTorchButton: false, // iOS and Android
+              showTorchButton: true, // iOS and Android
               torchOn: false, // Android, launch with the torch switched on (if available)
               saveHistory: true, // Android, save scan history (default false)
               prompt: "Place a barcode inside the scan area", // Android
               resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
               //formats: "QR_CODE,PDF_417,CODABAR,CODE_128,CODE_93,CODE_39", // default: all but PDF_417 and RSS_EXPANDED
-              orientation: "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+              //orientation: "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
               disableAnimations: true, // iOS
               disableSuccessBeep: false // iOS and Android
           }
