fe2584f6cd48e8267d5884e2703f4d21ff6e81f3
projpro <48902384+projpro@users.noreply.github.com>
2020-03-29 19:52:02 +0530

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -1402,6 +1402,7 @@ function InitPushNotification(storeId, name, uuid, version) {
     });
 
     push.on('notification', function (data) {
+        alert(data.message);
         //setTimeout(function () {
         //    stopAudio();
         //    acceptOrderPopup.destroy();
@@ -1418,6 +1419,55 @@ function InitPushNotification(storeId, name, uuid, version) {
             $("#btnAcknowledgement").click();
            
         }
+        else if (data.message == "Device Ping") {
+            alert("Ping");
+            $('#myDiv').html('<div class="block">' +
+                                             '<a href="#" class="link popup-close modal-accept-button"  id="btnAcknowledgement" onclick="StopSound();">Click To Stop Sound</a>' +
+                                             '<div class="overlay-button-area" id="dvPopOrders" style=\"top: 30px !important;\">' +
+                                             '</div>' +
+                                            '</div>');
+            $('#myDiv').show();
+
+            //Reference the Table.
+            var tblForm = document.getElementById("dvPopOrders");
+
+            //Reference all INPUT elements in the Table.
+            var inputs = document.getElementsByTagName("input");
+
+            //Loop through all INPUT elements.
+            for (var i = 0; i < inputs.length; i++) {
+                //Check whether the INPUT element is TextBox.
+                if (inputs[i].type == "text") {
+                    $(inputs[i])
+                   .putCursorAtEnd() // should be chainable
+                   .on("focus", function () { // could be on any event
+                       $(inputs[i]).putCursorAtEnd()
+                   });
+                    //Check whether Date Format Validation is required.
+                    if (inputs[i].className.indexOf("popup_date") != 1) {
+
+                        //Set Max Length.
+                        inputs[i].setAttribute("maxlength", 10);
+
+                        //Only allow Numeric Keys.
+                        inputs[i].onkeydown = function (e) {
+                            return IsNumeric(this, e.keyCode);
+                        };
+
+                        //Validate Date as User types.
+                        inputs[i].onkeyup = function (e) {
+                            ValidateDateFormat(this, e.keyCode);
+                        };
+                    }
+                }
+            }
+
+            if (isDevice()) {
+                // console.log('isDevice 1: ')
+                //playAudio();
+                myMedia.play();
+            }
+        }
         // alert('notification event: ' + data.message + ", " + data.title);
         //navigator.notification.alert(
         //    data.message,         // message
@@ -1427,6 +1477,14 @@ function InitPushNotification(storeId, name, uuid, version) {
         //);
     });
 }
+
+function StopSound() {
+    $('#myDiv').hide();
+    if (isDevice()) {
+        myMedia.stop();
+    }
+}
+
 function disableScrolling() {
     var x = window.scrollX;
     var y = window.scrollY;
