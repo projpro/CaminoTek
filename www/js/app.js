c16cc4a8ca49d9df8555249df6dce907b4fff945
projpro <48902384+projpro@users.noreply.github.com>
2020-06-10 01:40:32 +0530

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -342,58 +342,42 @@ $$(document).on('page:init', function (e) {
             //cordova.plugins.printer.print("Hello Document Direct Print", { printer: 'ipp://192.168.68.199' });
             var printerName = "";
 
-            BTPrinter.list(function (data) {
-                alert("List");
-                alert(data); //list of printer in data array
-                printerName = data[0];
-                alert("Printer: " + printerName);
-              //Connect 1  
-                BTPrinter.connect(function (data) {
-                alert("Connect1");
-                console.log(data);
-                   //Connect 1 Success
-                    //Print 1 Start
-                    BTPrinter.printText(function (data) {
-                alert("Success Print 1");
-                console.log(data);
-            }, function (err) {
-                alert("PrintText 1 Error");
-                console.log(err)
-            }, "Hello Print");
-                    //Print 1 End
-                    
-            }, function (err) {
-                alert("Connect1 Error: " + err);
-                console.log(err);
-            }, printerName);
-                //Connect 1 End
+            //BTPrinter.list(function (data) {
+                //alert("List");
+                //alert(data); //list of printer in data array
+                //printerName = data[0];
+                //alert("Printer: " + printerName);                                            
+            //}, function (err) {
+                //alert("List Error");
+                //console.log(err);
+            //});
+            
+            //BTPrinter.connect(function (data) {
+                //alert("Connect1");
+                //console.log(data);                    
+            //}, function (err) {
+                //alert("Connect1 Error: " + err);
+                //console.log(err);
+            //}, printerName);
+            
+            BTPrinter.connect(function (data) {
+                alert("Connect");
+                console.log(data);   
                 
-                //Connect 2 Start
-                BTPrinter.connect(function (data) {
-                alert("Connect 2");
-                console.log(data);
-                    //Connect 2 Success
-                        //Print 2 Start
-                    BTPrinter.printText(function (data) {
-                alert("Success Print 2");
+                //Connect Success Start
+                BTPrinter.printText(function (data) {
+                alert("Success Print");
                 console.log(data);
             }, function (err) {
-                alert("PrintText 2 Error");
-                console.log(err);
-            }, "Hello Print");
-                    //Print 2 End
+                alert("PrintText Error");
+                console.log(err)
+            }, "Hello Print");                
+                //Connect Success End
                     
             }, function (err) {
-                alert("Connect 2 Error: " + err);
+                alert("Connect Error: " + err);
                 console.log(err);
             }, "TCKP302-UB");
-                //Connect 2 End
-                
-            }, function (err) {
-                alert("List Error");
-                console.log(err);
-            });
-            
             
             
 
