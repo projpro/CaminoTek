54bcc43b12afc55b3fb774fa2ab4762be3d654e0
projpro <48902384+projpro@users.noreply.github.com>
2020-06-22 01:34:16 +0530

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -343,8 +343,11 @@ $$(document).on('page:init', function (e) {
                     }, function (err) {
                     }, "ABC" + "\n");
 
-                    BTPrinter.printPOSCommand(function () { }, function () { }, '\0x1B\0x40\0x1B\0x6b');
-                    BTPrinter.printPOSCommand(function () { }, function () { }, '\0x1D');
+                    //BTPrinter.printPOSCommand(function () { }, function () { }, '\0x1B\0x40\0x1B\0x6b');
+                    //BTPrinter.printPOSCommand(function () { }, function () { }, '\0x1D');
+                    BTPrinter.printPOSCommand(function(data){}, function(err){
+                    alert("printPOSCommand: " + err);
+                    }, "1D");
                     
 
                     BTPrinter.printText(function (data) {
