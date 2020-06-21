53208d25c978520970e4eace1439c48c5944a6f5
projpro <48902384+projpro@users.noreply.github.com>
2020-06-22 00:59:46 +0530

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -343,7 +343,7 @@ $$(document).on('page:init', function (e) {
                     }, function (err) {
                     }, "ABC" + "\n");
 
-                    //BTPrinter.printPOSCommand(function () { }, function () { }, '\x0A\x0A\x0A\x0A\x0A');
+                    BTPrinter.printPOSCommand(function () { }, function () { }, '\x0A\x0A\x0A\x0A\x0A');
 
                     BTPrinter.printText(function (data) {
                         alert("Print DEF");
@@ -354,11 +354,11 @@ $$(document).on('page:init', function (e) {
                     BTPrinter.disconnect(function (data) {
                         $('#btnPrintOrder').text("PRINT");
                         //alert("Disconnect");
-                        console.log(data)
+                        //console.log(data)
                     }, function (err) {
                         //alert("Disconnect Error");
                         $('#btnPrintOrder').text("PRINT");
-                        console.log(err)
+                        //console.log(err)
                     }, "TM-m30_003646");//TCKP302-UB
                 }, function (err) {
                     $('#btnPrintOrder').text("PRINT");
